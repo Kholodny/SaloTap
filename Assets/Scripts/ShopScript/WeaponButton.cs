9fd7131020a59d1c37cf352492ab2d0e627d194b
meepoha322 <bogatuk-alexey@yandex.ru>
2016-10-29 21:52:14 +0300

    Сортировка и описание скрипта WeaponButtons

--- Assets/Scripts/ShopScript/WeaponButton.cs
+++ Assets/Scripts/ShopScript/WeaponButton.cs
@@ -13,6 +13,13 @@ namespace CompleteProject
 		public Text cost;
 		public Text description;
 
+		/*
+		 * Добавить меню выбора предметов, куда перекидываются предметы и их там можно выбирать, а в магазине кнопку делать неактивной 
+		 * Скорее всего нужно это добавить в этом скрипте, используя условие покупки ->
+		 * ->Если предмет куплен, то в вашем сундуке вызывать функционал выбора его, а в магазине interectible = false
+		 * 
+		*/
+
 		//private AudioSource source;
 
 		// Use this for initialization
@@ -27,6 +34,8 @@ namespace CompleteProject
 			name.text = profile.weapons [weaponNumber].name;
 			cost.text = "$" + profile.weapons [weaponNumber].cost;
 			description.text = profile.weapons [weaponNumber].description;
+
+			//Здесь добавить условие, при котором если предмет куплен, то будет выводиться только его левел и урон, без цены
 		}
 
 		public void OnClick()
